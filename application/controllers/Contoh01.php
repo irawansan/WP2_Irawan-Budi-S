f9b05144059b98c87edaf924da7c0ee6e6d294cd
irawansan <irawansan007@gmail.com>
2024-04-01 22:50:09 +0700

    penambahan file

---
+++ application/controllers/Contoh01.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh01 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Irawan Budi Santoso
+        Saya tinggal di daerah Cakung, olahraga yang 
+        saya sukai adalah bulutangkis";
+    }
+
+} 
