4cc0df891be720f240643a6e87fee4646238bab8
irawansan <irawansan007@gmail.com>
2024-06-01 11:55:21 +0700

    menambahkan dan mengedit file file tugas

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Prog 2 | Merancang Template sederhana dengan codeigniter</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/stylebuku.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/csss/stylebuku.css">
    
 </head>
 <body>
